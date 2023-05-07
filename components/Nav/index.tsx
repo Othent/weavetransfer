90d0436fa891f8d1c71329bf628b728e788cfe46
Lorimer Jenkins <90644448+LorimerJenkins@users.noreply.github.com>
2023-05-07 00:51:32 -0400

    feat: new navbar

--- components/Nav/index.tsx
+++ components/Nav/index.tsx
@@ -1,8 +1,96 @@
 import * as Styled from './styles';
 import { DMSans700 } from '../../utils/fonts';
-import Button from '../Button'
+import Button from '../Button';
+import { Othent } from 'othent';
+import { useState, useEffect, useRef } from 'react';
 
 const Nav = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [othentInstance, setOthentInstance] = useState(null);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const dropdownRef = useRef(null);
+
+  useEffect(() => {
+    async function createOthentInstance() {
+      const othent = await Othent({ API_KEY: 'API_KEY', API_ID: 'API_ID' });
+      setOthentInstance(othent);
+    }
+    createOthentInstance();
+  }, []);
+
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setShowDropdown(false);
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [dropdownRef]);
+
+  const [userPicture, setUserPicture] = useState('');
+  const [userName, setUserName] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  const [userContractId, setUserContractId] = useState('');
+  async function logIn() {
+    const user_details = await othentInstance.logIn();
+    setUserPicture(user_details.picture);
+    setUserName(user_details.name)
+    setUserEmail(user_details.email)
+    setUserContractId(user_details.contract_id)
+    setIsLoggedIn(true);
+  }
+
+  async function logOut() {
+    await othentInstance.logOut();
+    setIsLoggedIn(false);
+    setUserPicture('');
+  }
+
+  function toggleDropdown() {
+    setShowDropdown(!showDropdown);
+  }
+
+
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
+  function showAccount() {
+    setShowDropdown(false);
+    setIsPopupOpen(true);
+  }
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (event.target.classList.contains('popup-background')) {
+        setIsPopupOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('click', handleClickOutside);
+    };
+  }, []);
+
+  
+  const [copyClicked, setCopyClicked] = useState(false);
+  const handleCopy = () => {
+    setCopyClicked(true);
+    navigator.clipboard.writeText(userContractId);
+    setTimeout(() => {
+      setCopyClicked(false);
+    }, 100);
+  };
+
+
+
+
+  
+
+
   return (
     <Styled.NavBar>
       <Styled.NavLogo>
@@ -13,16 +101,85 @@ const Nav = () => {
       </Styled.NavLogo>
 
       <Styled.Menu>
-        {/* <a href='https://mobile.othent.io' target='_blank' className={`${DMSans700.className} devs`}>
-          Mobile
-        </a> */}
-        <a href='https://docs.othent.io/developers/sdk' target='_blank' className={`${DMSans700.className} devs`}>
+        <a href='https://github.com/Othent/weavetransfer' 
+        target='_blank' 
+        className={`${DMSans700.className} devs`}>
           Developers
         </a>
-        {/* <a href='https://blog.othent.io' target='_blank' className={DMSans700.className}>
-          Blog
-        </a> */}
-        <Button href='mailto:hello@othent.io'>Contact Us</Button>
+        <a href='mailto:hello@othent.io' className={DMSans700.className}>
+          Contact Us
+        </a>
+        {isLoggedIn ? (
+          <div ref={dropdownRef}>
+            <Styled.UserImgContainer onClick={() => toggleDropdown()}>
+                <Styled.DropdownArrow 
+                src="./drop_down_arrow.svg" 
+                alt='Dropdown icon' 
+                referrerPolicy='no-referrer' />
+                <Styled.userImg src={userPicture} alt='User picture' referrerPolicy='no-referrer' />
+            </Styled.UserImgContainer>
+            {showDropdown && (
+              <Styled.Dropdown>
+                <Styled.DropdownItem 
+                onClick={() => showAccount()}>
+                  Account
+                </Styled.DropdownItem>
+                <Styled.DropdownItem 
+                onClick={() => logOut()}>
+                  Log Out
+                </Styled.DropdownItem>
+              </Styled.Dropdown>
+            )}
+          </div>
+        ) : (
+          <Button onClick={() => logIn()}>Sign in / up</Button>
+        )}
+
+        {isPopupOpen && (
+          <>
+          <Styled.BlurredBody className="popup-background" />
+
+          <Styled.Popup>
+            <Styled.PopupHeaderContainer>
+              <Styled.PopupHeader>
+                {userName}
+              </Styled.PopupHeader>
+              <Styled.PopupCloseButton onClick={() => setIsPopupOpen(false)}>
+                Close
+              </Styled.PopupCloseButton>
+            </Styled.PopupHeaderContainer>
+    
+            <Styled.PopupBody>
+              <Styled.UserPicture src={userPicture} alt="User picture" referrerPolicy='no-referrer' />
+              <Styled.UserEmail>{userEmail}</Styled.UserEmail>
+    
+              <Styled.UserContractIdContainer>
+                <Styled.UserContractId className="user-contract-id">
+                  {userContractId}
+                </Styled.UserContractId>
+                <Styled.UserContractIdCopy
+                  src="./copy.svg"
+                  alt="Copy contract ID"
+                  onClick={handleCopy}
+                  referrerPolicy='no-referrer'
+                  style={{ filter: copyClicked ? "grayscale(100%) brightness(0%)" : "none" }}
+                />
+              </Styled.UserContractIdContainer>
+    
+              <Styled.ViewTransactionsButton 
+                href={`https://sonar.warp.cc/#/app/contract/${userContractId}`} 
+                target="_blank"
+              >
+                View Recent Transactions
+              </Styled.ViewTransactionsButton>
+            </Styled.PopupBody>
+          </Styled.Popup>
+
+        </>
+        )}
+
+
+
       </Styled.Menu>
     </Styled.NavBar>
   );
--- components/Nav/styles.ts
+++ components/Nav/styles.ts
@@ -97,3 +97,149 @@ export const Menu = styled.nav`
     }
   }
 `;
+
+
+
+export const UserImgContainer = styled.div`
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+`;
+
+export const userImg = styled.img`
+  height: 32px;
+  width: 32px;
+  object-fit: cover;
+  border-radius: 50%;
+  cursor: pointer;
+`;
+
+export const DropdownArrow = styled.img`
+  margin-right: 5px;
+  height: 7px;
+`;
+
+export const Dropdown = styled.div`
+  position: absolute;
+  top: calc(90%);
+  right: 2rem;
+  background-color: #ffffff;
+  box-shadow: 0px 2px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 5px;
+  z-index: 10;
+`;
+
+export const DropdownItem = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  text-align: center;
+  font-size: 16px;
+  color: #333333;
+  padding: 8px 20px;
+  cursor: pointer;
+  transition: background-color 0.23s ease-in-out;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+`;
+
+
+
+
+
+export const BlurredBody = styled.div`
+  height: 100vh;
+  width: 100vw;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background: rgba(255, 255, 255, 0.8);
+  backdrop-filter: blur(5px);
+  z-index: 999;
+`;
+
+
+export const Popup = styled.div`
+  position: fixed;
+  top: 500%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 400px;
+  box-shadow: 0px 5px 10px rgba(0, 0, 0, 0.2);
+  z-index: 999;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin: 1rem;
+  padding: 2rem;
+  border-radius: 16px;
+  border: 1px solid #d3e3fc;
+  background: white;
+`;
+export const PopupHeaderContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 2rem;
+`;
+export const PopupHeader = styled.h2`
+  font-size: 24px;
+  font-weight: bold;
+`;
+export const PopupCloseButton = styled(Button)`
+  margin-top: auto;
+`;
+
+
+
+
+export const PopupBody = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+`;
+export const UserPicture = styled.img`
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  object-fit: cover;
+  margin-bottom: 1rem;
+`;
+export const UserEmail = styled.p`
+  font-size: 16px;
+  color: #666;
+  margin-bottom: 0.5rem;
+`;
+
+export const UserContractIdContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+export const UserContractId = styled.p`
+  font-size: 16px;
+  color: #666;
+  word-break: break-all;
+  text-align: center;
+  width: 70%;
+`;
+export const UserContractIdCopy = styled.img`
+  height: 15px;
+  margin-left: 10px;
+  cursor: pointer;
+`
+
+
+
+export const ViewTransactionsButton = styled(Button)`
+  margin-top: 2rem;
+`;
+
